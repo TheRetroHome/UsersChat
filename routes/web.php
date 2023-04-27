438e2c8bb3a41914dff058dc900cdb7085ec29e1
TheRetroHome <kenni.iishchenko@mail.ru>
2023-04-28 00:48:26 +0500

    Настроены шаблоны
    -Настроил основной шаблон, а также регистрации и авторизации с использованием bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[HomeController::class,'index'])->name('home');
+
+Route::get('/register',[AuthController::class,'registerForm'])->name('register.create');
+Route::get('/login',[AuthController::class,'loginForm'])->name('login.create');
